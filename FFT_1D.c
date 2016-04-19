757dd495b116283f2fdba2ddafb2863d8ea1a54e
Monil Jhaveri <moniljhaveri@30-39-41-155-wireless1x.bu.edu>
2016-04-19 18:26:03 -0400

    2d is not working yet

--- FFT_1D.c
+++ FFT_1D.c
@@ -5,12 +5,12 @@
 #include "stb_image_write.h" 
 const int N = 128; 
 
-double fRe[N]; 
-double fIm[N]; 
-double fAmp[N]; 
-double FRe[N]; 
-double FIm[N]; 
-double FAmp[N]; 
+double fRe[N][N][3]; 
+double fIm[N][N][3]; 
+double fAmp[N][N][3]; 
+double FRe[N][N][3]; 
+double FIm[N][N][3]; 
+double FAmp[N][N][3]; 
 const double pi = 3.1415926535897932384626433832795; 
 
 typedef int bool; 
@@ -18,26 +18,28 @@ typedef int bool;
 #define false 0 
 
 
-int width, height; 
+int width, height, bpp; 
 
 
-void FFT(int n, bool inverse, const double *gRe, const double *gIm, double *GRe, double *GIm){
+void FFT(int n, bool inverse, double *gRe, double *gIm, double *GRe, double *GIm, int stride, double factor){
 	int m = 0; 
 	int p = 1; 
+
 	int i = 0; 
+
 	while (p < n){
 		p *= 2; 
 		m++; 
 	} 
 	
 	//Bit reversal 
-	GRe[n - 1] = gRe[n - 1];
-	GIm[n - 1] = gIm[n - 1]; 
+	GRe[(n - 1) * stride] = gRe[(n - 1) * stride];
+	GIm[(n - 1) * stride] = gIm[(n - 1) * stride];
 
 	int j = 0; 	
 	for(i = 0; i < n - 1; i++){
-		GRe[i] = gRe[j]; 
-		GIm[i] = gIm[j]; 
+		GRe[i * stride] = gRe[j * stride]; 
+		GIm[i * stride] = gIm[j * stride]; 
 		int k = n/2; 
 		while(k <= j){
 			j -= k; 
@@ -45,6 +47,8 @@ void FFT(int n, bool inverse, const double *gRe, const double *gIm, double *GRe,
 		} 
 		j += k; 
 	} 
+	
+	//Calculates FFT 
 
 	double ca = -1.0; 
 	double sa = 0.0; 
@@ -60,12 +64,12 @@ void FFT(int n, bool inverse, const double *gRe, const double *gIm, double *GRe,
 		for ( j = 0; j < l1; j++){
 			for ( i = j; i < n; i += l2){
 				int il = i + l1; 
-				double t1 = u1 * GRe[il] - u2 * GIm[il]; 
-				double t2 = u1 * GIm[il] - u2 * GRe[il]; 
-				GRe[il] = GRe[i] - t1; 
-				GIm[il] = GIm[i] - t2; 
-				GRe[i] = t1; 
-				GIm[i] = t2; 
+				double t1 = u1 * GRe[il * stride] - u2 * GIm[il * stride]; 
+				double t2 = u1 * GIm[il * stride] - u2 * GRe[il * stride]; 
+				GRe[il * stride] = GRe[i * stride] - t1; 
+				GIm[il * stride] = GIm[i * stride] - t2; 
+				GRe[i * stride] += t1; 
+				GIm[i * stride] += t2; 
 				
 
 			} 
@@ -75,41 +79,50 @@ void FFT(int n, bool inverse, const double *gRe, const double *gIm, double *GRe,
 
 		} 
 		sa = sqrt((1.0 - ca)/2.0); 
-		if ( !inverse) sa = -sa; 
+		if ( !inverse) sa =- sa; 
 		ca = sqrt((1.0 + ca)/2.0); 
 	} 
 	if (!inverse){
 		int i = 0; 
 		for (i = 0; i < n; i++){
-			GRe[i] /= n; 
-			GIm[i] /= n; 
+			GRe[i * stride] /= factor; 
+			GIm[i * stride] /= factor; 
 		} 
 	} 
 
 
 
 }  
+void load(double *fRe, double *rIm){
+	unsigned char* data; 
+	//unsigned int* mod; 
+
+	data = stbi_load("test.png", &width, &height, &bpp, STBI_rgb_alpha); 
+
+} 
 
 int main(){
 	
-	int i; 
+	int i, j ; 
 
 	for (i = 0; i < N; i++){
-		double x = 25 * sin(i /2.0);
-		fRe[i] = x; 
-		fIm[i] = x;  
-		printf("%f \n", fRe[i]); 
+		for ( j = 0; j < N; j++){
+			double x = 25 * sin(i /2.0);
+			fRe[i][j][0] = x; 
+			fIm[i][j][0] = x; 
+			fIm[i][j][1] = x;  
+			fRe[i][j][1] = x; 
+			fRe[i][j][2] = x;  
+			fIm[i][j][2] = x;  
+		
+		} 
 
 	} 
-	FFT(N, 0, fRe, fIm, FRe, FIm); 
-		printf("\n FFT Real \n" ); 
-	FFT(N, 0, FRe, FIm, fRe, fIm); 
-	for (i = 0; i < N; i++){
-		printf("%f \n", fRe[i]); 
-	} 
-	int width, height, bpp; 
-	unsigned char* rgb = stbi_load("test.png", &width, &height, &bpp, 3); 
-	printf("This works \n"); 
-	stbi_image_free(rgb); 
+	FFT(N, 0, fRe, fIm, FRe, FIm, 3, 1); 
+
+
+	//int width, height, bpp; 
+	//printf("This works \n"); 
+	//stbi_image_free(rgb); 
 	return 0; 
 } 
